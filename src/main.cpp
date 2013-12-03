d80593760084b6e6ac4497f4638bb3f6cce9400b
Wei-Wei Lu <tweilu@ubuntu.(none)>
2013-12-02 22:38:25 -0500

    set up initial project with vecmath.

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Globals here.
+namespace
+{
+
+}
+
+// Main routine.
+// Set up OpenGL, define the callbacks and start the main loop
+int main( int argc, char* argv[] )
+{
+    cout << "Hello World";
+}
